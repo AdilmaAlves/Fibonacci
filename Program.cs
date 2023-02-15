6a9ce82702f9a22cdadf2656805fad519c9f8544
Adilma Alves <125414087+AdilmaAlves@users.noreply.github.com>
2023-02-14 22:12:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 1, b = 1, c = 1;  
+    Console.Write("{0} {1}", a,b);  
+    for (int i = 2; i < 20; i++)  
+    {  
+        c= a + b;  
+        Console.Write(" {0}", c);  
+        a= b;  
+        b= c;  
+    }  
